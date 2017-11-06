6980d8ce758129192ab4ffa6e4d3d3aaf2bbbd61
Malcolm <malcolmfinlayson81@gmail.com>
2017-11-06 23:41:19 +0000

    All tests and classes completed - Library class still to test

---
+++ library_class.rb
@@ -0,0 +1,39 @@
+class Library
+    def initialize(library_arr)
+      @library = library_arr
+    end
+
+    attr_reader :library
+    attr_accessor
+
+    #Could fancify the output
+    def library_summary
+      lib_sum_arr = @library.map { |book|
+      book[:title]  }
+      return lib_sum_arr
+    end
+
+    #Could fancify the output
+    def book_details(book_str)
+      have_book = library_summary.includes?(book_str)
+      return "Sorry, this book was not found" unless have_book
+      book_obj = @library.select { |book|
+      book[:title] == book_str
+      return book_obj
+    end
+
+    #Could fancify the output
+    def book_rental_details(book_str)
+      have_book = library_summary.includes?(book_str)
+      return "Sorry, this book was not found" unless have_book
+      return book_details(book_str)[:rental_details]
+    end
+
+
+    def add_book(*book_objs_arr)
+      book_objs_arr.each {|book_obj|
+        @library.push(book_obj)
+      }
+      return "#{book_objs_arr.count} books added to library"
+    end
+end
--- specs/library_class_spec.rb
+++ specs/library_class_spec.rb
@@ -3,8 +3,114 @@ require ('minitest/rg')
 require_relative ('../library_class.rb')
 
 class LibraryClassTests < MiniTest::Test
+  def initialize
+    @library_hash = [
+      {
+        title: "lord_of_the_rings",
+        rental_details: {
+         student_name: "Jeff",
+         date: "01/12/16"
+        }
+      },
+      {
+        title: "1984",
+        rental_details: {
+         student_name: "Dave",
+         date: "24/05/16"
+        }
+      },
+      {
+        title: "zen_and_the_art_of_motorcyle_maintainance",
+        rental_details: {
+         student_name: "Karen",
+         date: "23/8/16"
+        }
+      }
+    ]
 
-  def test_
+    @new_book =   {
+        title: "the_very_hungry_catapillar",
+        rental_details: {
+         student_name: "George",
+         date: "17/03/01"
+        }
+      }
+  end
+
+
+  def test_get_library_detailed
+    #ARRANGE
+    library_1 = Library.new(@library_hash)
+    #ACT
+    #ASSERT
+    assert_equal(@library_hash, library_1.library)
+  end
+
+
+  def test_get_library__summary
+    #ARRANGE
+    library_1 = Library.new(@library_hash)
+    #ACT
+    result = library_1.library_summary
+    #ASSERT
+    assert_equal(["lord_of_the_rings','1984','zen_and_the_art_of_motorcyle_maintainance"], result)
+  end
+
+
+  def test_query_book_details__found
+    #ARRANGE
+    library_1 = Library.new(@library_hash)
+    #ACT
+    result = library_1.book_rental_details('1984')
+    #ASSERT
+    assert_equal({
+      title: "zen_and_the_art_of_motorcyle_maintainance",
+      rental_details: {
+       student_name: "Karen",
+       date: "23/8/16"
+       }, result)
+  end
+
+
+  def test_query_book_details__not_found
+    #ARRANGE
+    library_1 = Library.new(@library_hash)
+    #ACT
+    result = library_1.book_details('Not a book')
+    #ASSERT
+    assert_equal("Sorry, this book was not found", result)
+  end
+
+
+  def test_query_book_rental_details__found
+    #ARRANGE
+    library_1 = Library.new(@library_hash)
+    #ACT
+    result = library_1.book_rental_details('1984')
+    #ASSERT
+    assert_equal({
+       student_name: "Dave",
+       date: "24/05/16"
+    }, result)
+  end
+
+  def test_query_book_rental_details__not_found
+    #ARRANGE
+    library_1 = Library.new(@library_hash)
+    #ACT
+    result = library_1.book_rental_details('Not a book')
+    #ASSERT
+    assert_equal("Sorry, this book was not found", result)
+  end
+
+  def test_add_book
+    #ARRANGE
+    library_1 = Library.new(@library_hash)
+    #ACT
+    library_1.add_book(@new_book)
+    result = library_1.library.count
+    #ASSERT
+    assert_equal(4, result)
   end
 
 end
